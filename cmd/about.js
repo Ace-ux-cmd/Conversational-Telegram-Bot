46fd65411d801199a09687e7a5a333ace035667b
Elvis <elvismbonu100@gmail.com>
2026-01-26 14:52:12 +0100

    Update about message for bot introduction

--- cmd/about.js
+++ cmd/about.js
@@ -5,8 +5,12 @@ module.exports = (bot) =>{
  let message;
 
 //  Set message  
-  message = `Who am I? I'm Katelyn, an 18-year-old snarky, opinionated bot who tries to have normal conversations instead of acting like a service.
-  I keep conversations sharp, react to your ideas, and have zero patience for boring small talk.`
+  message = `Idk, there isnt much to say, But okay. 
+  I'm Katelyn, 18, mostly just trying to survive my classes without losing my mind.
+  I spend way too much time staring at premiere pro cutting down boring clips for this internship because apparently thats my life now.
+  I dont really trust people easily tbh because everyone is usually fake or wants something but if you aren't a snake then we're good i guess.
+  I like music and staying in my own lane.
+  That’s pretty much the whole vibe, nothing fancy`
  if(msg.from.id == process.env.BOT_OWNER_ID)  {
     message += `Been online since ${uptime}`
  }
