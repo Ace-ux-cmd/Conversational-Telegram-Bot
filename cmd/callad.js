ed08538fef844563eaf45e7f55d78fe3856e3551
Elvis <elvismbonu100@gmail.com>
2026-01-26 11:31:36 +0100

    Add WhatsApp and Facebook message buttons

--- cmd/callad.js
+++ cmd/callad.js
@@ -6,6 +6,8 @@ module.exports = (bot)=>{
             inline_keyboard:[
                 [{text: "Message Elvis", url:"https://t.me/chidalumb100"}],
                 [{text: "Message Ace'", url:"https://t.me/elvismb10"}],
+                [{ text: "📱🟢💬 Message Admin on WhatsApp", url: "https://wa.me/+2347054971517" }],
+                [{ text: "📘💬 Add on Facebook", url: "https://www.facebook.com/profile.php?id=61578323177234" }]
             ]
         }
     })
