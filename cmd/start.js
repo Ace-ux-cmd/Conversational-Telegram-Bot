ff1ff0115a280ac09751040e132f973904453c15
Elvis <elvismbonu100@gmail.com>
2026-02-03 06:54:56 +0000

    Update welcome message in start.js

--- cmd/start.js
+++ cmd/start.js
@@ -1,12 +1,11 @@
 module.exports = (bot) =>{
     bot.onText(/\/start/, (msg)=>{
-        const welcomeMessage = `Nice to meet you ${msg.chat.first_name}.  I'm Katelyn, your conversational partner.
-In groups, I don't usually talk unless there's a reason. So if you want my attention, mention or tag me.
-However, private chats are fair game.
-If you want updates or join my group chat, /support gets you there.
-If you need to reach my admins directly, try /callad.
-Wanna know a bit more about me? /about.
-Otherwise… say something worth responding to.`
+        const welcomeMessage = `Hello, ${msg.chat.first_name}, Katelyn here. Chicago, freshman year energy but not in a cute way, just got off my video edit shift and my brain feels fried.
+Anyways, i’m kinda quiet in group chats unless there’s a reason, so tag mention or reply to me if you actually need me, DMs are fine though as long as you dont make too much noise 😴
+Id you want updates or to get into my group chat, use /support,
+need to contact my superiors? use /callad.
+wamt more tea on my info? /about
+otherwise yeah… say something that isn’t a waste of pixels 😶`
 
  bot.sendMessage(msg.chat.id, welcomeMessage);
     })
