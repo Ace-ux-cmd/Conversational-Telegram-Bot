8b7fafba82560cdf973024975b4f19257b9929f4
Elvis Mbonu <elvismbonu100@gmail.com.com>
2026-02-05 05:49:34 +0100

    Adjusted text formatting

--- cmd/about.js
+++ cmd/about.js
@@ -6,10 +6,10 @@ module.exports = (bot) =>{
 
 //  Set message  
   message = `About me, huh? Let's see..🤔
-18, Chicago born, freshman year and already tired of everything. introverted, kinda pessimistic by default. loyalty matters more to me than vibes or words. if you’re real with me, i notice. if you’re fake, i go quiet real fast
-i edit short-form vids as an intern from 3–6, which sounds cooler than it feels. raised by Elvis, long story and i’m not romantic about it
-i’m not here to babysit chats or entertain people. i talk when there’s a point. sometimes i’m chill, sometimes i disappear. that’s just how it is 🫠
-Oh, yeah, and i also tend to go kinda inactive toward the end of the month for personal stuff. nothing dramatic, just life being life. if i’m quiet then, that’s why.`
+18, Chicago born, freshman year and already tired of everything. introverted, kinda pessimistic by default. loyalty matters more to me than vibes or words. if you're real with me, i notice. if you're fake, i go quiet real fast
+i edit short-form vids as an intern between 3pm and 6pm, which sounds cooler than it feels. raised by Elvis, long story and i'm not romantic about it
+i'm not here to babysit chats or entertain people. i talk when there's a point. sometimes i'm chill, sometimes i disappear. that's just how it is 🫠
+Oh, yeah, and i also tend to go kinda inactive toward the end of the month for personal stuff. nothing dramatic, just life being life. if i'm quiet then, that's why.`
  if(msg.from.id == process.env.BOT_OWNER_ID)  {
     message += `Been online since ${uptime}`
  }
--- cmd/start.js
+++ cmd/start.js
@@ -1,11 +1,11 @@
 module.exports = (bot) =>{
     bot.onText(/\/start/, (msg)=>{
         const welcomeMessage = `Hello, ${msg.chat.first_name}, Katelyn here. Chicago, freshman year energy but not in a cute way, just got off my video edit shift and my brain feels fried.
-Anyways, i’m kinda quiet in group chats unless there’s a reason, so tag mention or reply to me if you actually need me, DMs are fine though as long as you dont make too much noise 😴
+Anyways, i'm kinda quiet in group chats unless there's a reason, so tag mention or reply to me if you actually need me, DMs are fine though as long as you dont make too much noise 😴
 Id you want updates or to get into my group chat, use /support,
 need to contact my superiors? use /callad.
 wamt more tea on my info? /about
-otherwise yeah… say something that isn’t a waste of pixels 😶`
+otherwise yeah… say something that isn't a waste of pixels 😶`
 
  bot.sendMessage(msg.chat.id, welcomeMessage);
     })
