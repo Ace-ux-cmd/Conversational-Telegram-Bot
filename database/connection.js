b5cc7bc42235d62a403f4059995e669f9941e322
Elvis <elvismbonu100@gmail.com>
2026-06-07 10:23:25 +0100

    Initial release V2.0.0
    Removed SSL configuration to reject unauthorized connections.

--- database/connection.js
+++ database/connection.js
@@ -2,9 +2,6 @@ const { Pool } = require("pg");
 
 const pool = new Pool({
     connectionString: process.env.CONNECTION_STRING,
-    ssl: {
-        rejectUnauthorized: false
-    },
     connectionTimeoutMillis: 30000,
     idleTimeoutMillis: 20000,
     max: 10
